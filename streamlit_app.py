2d2eac27a5bb37428ae0b0b92a0e65049880c983
tanjusay <112522489+tanjusay@users.noreply.github.com>
2023-06-05 15:50:49 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import requests
 from PIL import Image
 import streamlit as st
 import matplotlib.pyplot as plt
+from io import BytesIO
 from dotenv import load_dotenv
 
 # Load environment variables from .env file
@@ -11,6 +12,7 @@ load_dotenv()
 # CoinRanking API endpoint and headers
 API_ENDPOINT = "https://api.coinranking.com/v2/coins"
 HEADERS = {"x-access-token": os.getenv("COINRANKING_API_KEY")}
+
 
 # Function to get cryptocurrency data
 def get_crypto_data(crypto_name):
@@ -22,14 +24,16 @@ def get_crypto_data(crypto_name):
             return data["data"]["coins"][0]
     return None
 
-# Function to get cryptocurrency icon URL
-def get_crypto_icon_url(crypto_name):
-    response = requests.get(f"{API_ENDPOINT}/{crypto_name}")
+
+def get_crypto_icon(crypto):
+    response = requests.get(f"https://api.coinranking.com/v2/coins/{crypto['id']}")
     if response.status_code == 200:
         data = response.json()
         if data["status"] == "success":
-            return data["data"]["coin"]["iconUrl"]
+            crypto_icon_url = data["data"]["coin"]["iconUrl"]
+            return crypto_icon_url
     return None
+
 
 def display_sidebar():
     crypto_names = ["bitcoin", "ethereum", "litecoin"]  # Add more cryptocurrency names as needed
@@ -39,33 +43,45 @@ def display_sidebar():
 
     return selected_crypto
 
+
+# Function to display cryptocurrency data in main section
 def display_main_section(crypto_name):
     crypto_data = get_crypto_data(crypto_name)
 
     # Display cryptocurrency name and image
     st.title(crypto_name.capitalize())
-    crypto_icon_url = get_crypto_icon_url(crypto_name)
-    if crypto_icon_url:
-        response = requests.get(crypto_icon_url)
-        if response.status_code == 200:
-            icon = Image.open(BytesIO(response.content))
-            st.image(icon, caption=crypto_name.capitalize(), use_column_width=True)
+    crypto_icon = get_crypto_icon(crypto_data)
+    if crypto_icon:
+        image_response = requests.get(crypto_icon)
+        if image_response.status_code == 200:
+            image = Image.open(BytesIO(image_response.content))
+            st.image(image, caption=crypto_name.capitalize(), use_column_width=True)
 
     # Display bar chart of cryptocurrency prices
-    st.subheader("Price Chart")
-    fig, ax = plt.subplots()
-    ax.bar(crypto_data["history"], crypto_data["price"])
-    ax.set_xlabel("Date")
-    ax.set_ylabel("Price")
-    ax.set_title(f"{crypto_name.capitalize()} Price Chart")
-    plt.xticks(rotation=45)
-    st.pyplot(fig)
+    if crypto_data:
+        st.subheader("Price Chart")
+        history = crypto_data.get("history", [])
+        if history:
+            dates = []
+            prices = []
+            for item in history:
+                dates.append(item["date"])
+                prices.append(item["price"])
+            fig, ax = plt.subplots()
+            ax.bar(dates, prices)
+            ax.set_xlabel("Date")
+            ax.set_ylabel("Price")
+            ax.set_title(f"{crypto_name.capitalize()} Price Chart")
+            plt.xticks(rotation=45)
+            st.pyplot(fig)
+
 
 def main():
     st.set_page_config(page_title="Cryptocurrency Dashboard")
 
     selected_crypto = display_sidebar()
     display_main_section(selected_crypto)
+
 
 if __name__ == "__main__":
     main()
