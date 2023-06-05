9c33b647421faef0d80e86a50e62c76766ac709c
tanjusay <112522489+tanjusay@users.noreply.github.com>
2023-06-05 16:10:07 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,10 +43,10 @@ def display_main_section(crypto_name):
 
         # Display bar chart of cryptocurrency prices
         st.subheader("Price Chart")
-        history = crypto_data["history"]
+        history = crypto_data["historyPrices"]
         if history:
-            dates = [item["timestamp"] for item in history]
-            prices = [item["price"] for item in history]
+            dates = [item[0] for item in history]
+            prices = [item[1] for item in history]
             fig, ax = plt.subplots()
             ax.bar(dates, prices)
             ax.set_xlabel("Date")
