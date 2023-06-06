51a80ef072c9f8bf36f2daaf520a86ad861bc929
tanjusay <112522489+tanjusay@users.noreply.github.com>
2023-06-06 09:16:19 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,69 +4,44 @@ import streamlit as st
 import matplotlib.pyplot as plt
 from dotenv import load_dotenv
 
-# Load environment variables from .env file
+
 load_dotenv()
 
-# CoinRanking API endpoint and headers
 API_ENDPOINT = "https://api.coinranking.com/v2/coins"
 HEADERS = {"x-access-token": os.getenv("COINRANKING_API_KEY")}
 
 
-# Function to get cryptocurrency data
-def get_crypto_data(crypto_name):
-    params = {"symbol": crypto_name}
-    response = requests.get(API_ENDPOINT, headers=HEADERS, params=params)
+# fetch cryptocurrency data
+def get_crypto_data():
+    response = requests.get(API_ENDPOINT)
     if response.status_code == 200:
         data = response.json()
-        if data["status"] == "success" and data["data"]["stats"]["total"] > 0:
-            st.write(data)
-            return data["data"]["coins"][0]
-    return None
+        if data["status"] == "success":
+            return data["data"]["coins"]
+    return []
 
-
-def display_sidebar():
-    crypto_names = ["bitcoin", "ethereum", "litecoin"]  # Add more cryptocurrency names as needed
-
-    st.sidebar.title("Cryptocurrencies")
-    selected_crypto = st.sidebar.selectbox("Select a cryptocurrency", crypto_names)
-
-    return selected_crypto
-
-
-# Function to display cryptocurrency data in main section
-def display_main_section(crypto_name):
-    crypto_data = get_crypto_data(crypto_name)
-
-    if crypto_data:
-        # Display cryptocurrency name and image URL
-        st.title(crypto_data["name"])
-        st.image(crypto_data["iconUrl"], caption=crypto_data["name"], use_column_width=True)
-
-        # Display bar chart of cryptocurrency prices
-        st.subheader("Price Chart")
-        history = crypto_data.get("history")
-        if history:
-            dates = []
-            prices = []
-            for item in history:
-                dates.append(item["timestamp"])
-                prices.append(item["price"])
-            fig, ax = plt.subplots()
-            ax.bar(dates, prices)
-            ax.set_xlabel("Date")
-            ax.set_ylabel("Price")
-            ax.set_title(f"{crypto_data['name']} Price Chart")
-            plt.xticks(rotation=45)
-            st.pyplot(fig)
-
-
+# display cryptocurrency data
+def display_crypto_data(crypto_data):
+    for crypto in crypto_data:
+        st.title(f"{crypto['name']} ({crypto['symbol']})")
+        st.image(crypto['iconUrl'], caption=crypto['name'], use_column_width=True)
+        st.subheader("Statistics")
+        st.text(f"Market Cap: {crypto['marketCap']}")
+        st.text(f"Price: {crypto['price']}")
+        st.text(f"24h Volume: {crypto['24hVolume']}")
+        st.subheader("Sparkline")
+        sparkline = [float(price) for price in crypto['sparkline'].values()]
+        plt.plot(sparkline)
+        st.pyplot(plt)
 
 def main():
-    st.set_page_config(page_title="Cryptocurrency Dashboard")
+    st.set_page_config(page_title="Crypto Dashboard")
 
-    selected_crypto = display_sidebar()
-    display_main_section(selected_crypto)
-
+    crypto_data = get_crypto_data()
+    if crypto_data:
+        display_crypto_data(crypto_data)
+    else:
+        st.error("Failed to fetch cryptocurrency data.")
 
 if __name__ == "__main__":
     main()
