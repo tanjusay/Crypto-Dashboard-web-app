8dc3862c23a211d19d78339bfe0cd7579e69f6ee
tanjusay <112522489+tanjusay@users.noreply.github.com>
2023-06-06 09:19:25 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,44 +4,50 @@ import streamlit as st
 import matplotlib.pyplot as plt
 from dotenv import load_dotenv
 
-
+# Load environment variables from .env file
 load_dotenv()
 
+# CoinRanking API endpoint and headers
 API_ENDPOINT = "https://api.coinranking.com/v2/coins"
 HEADERS = {"x-access-token": os.getenv("COINRANKING_API_KEY")}
 
-
-# fetch cryptocurrency data
-def get_crypto_data():
-    response = requests.get(API_ENDPOINT)
+# Function to get cryptocurrency data
+def get_crypto_data(crypto_name):
+    params = {"symbol": crypto_name}
+    response = requests.get(API_ENDPOINT, headers=HEADERS, params=params)
     if response.status_code == 200:
         data = response.json()
         if data["status"] == "success":
-            return data["data"]["coins"]
-    return []
+            return data["data"]["coins"][0]
+    return None
 
-# display cryptocurrency data
-def display_crypto_data(crypto_data):
-    for crypto in crypto_data:
-        st.title(f"{crypto['name']} ({crypto['symbol']})")
-        st.image(crypto['iconUrl'], caption=crypto['name'], use_column_width=True)
-        st.subheader("Statistics")
-        st.text(f"Market Cap: {crypto['marketCap']}")
-        st.text(f"Price: {crypto['price']}")
-        st.text(f"24h Volume: {crypto['24hVolume']}")
-        st.subheader("Sparkline")
-        sparkline = [float(price) for price in crypto['sparkline'].values()]
-        plt.plot(sparkline)
-        st.pyplot(plt)
+# Function to display cryptocurrency data
+def display_crypto_data(crypto):
+    st.title(crypto['name'].capitalize())
+
+    # Display cryptocurrency icon
+    st.image(crypto['iconUrl'], caption=crypto['name'].capitalize(), use_column_width=True)
+
+    # Display cryptocurrency price
+    st.subheader("Price")
+    st.write(crypto['price'])
+
+    # Display sparkline chart
+    st.subheader("Sparkline")
+    sparkline = [float(price) for price in crypto['sparkline']]
+    plt.plot(sparkline)
+    st.pyplot()
 
 def main():
-    st.set_page_config(page_title="Crypto Dashboard")
+    st.set_page_config(page_title="Cryptocurrency Dashboard")
 
-    crypto_data = get_crypto_data()
+    crypto_name = "bitcoin"  # Replace with the desired cryptocurrency name
+    crypto_data = get_crypto_data(crypto_name)
+
     if crypto_data:
         display_crypto_data(crypto_data)
     else:
-        st.error("Failed to fetch cryptocurrency data.")
+        st.error("Failed to retrieve cryptocurrency data.")
 
 if __name__ == "__main__":
     main()
